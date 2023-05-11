302732657450d169dbceaa4866eb4a0a5fb34d39
md tarikh khan <97981490+DBcoppr@users.noreply.github.com>
2023-05-11 12:46:18 +0530

    Create problem1.js

---
+++ problem1.js
@@ -0,0 +1,15 @@
+var k=1
+var n=10 //input val- no of rows you want print
+for (let i=1;i<=n;i++){
+    let rw=""
+    for (let j=1;j<=i;j++){
+        if (i%2 === 0){
+            rw=k+" "+rw
+        }
+        else{
+            rw=rw?(rw+" "+k):rw+k
+        }
+        k++
+    }
+    console.log(rw)
+}
