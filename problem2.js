b02d5e0065bc8294441c9b1af64a3b2dd79e3280
md tarikh khan <97981490+DBcoppr@users.noreply.github.com>
2023-05-11 13:12:49 +0530

    Update problem2.js

--- problem2.js
+++ problem2.js
@@ -1,20 +1,20 @@
 //i decide row value and j decide column val
 //depending upon value of i we choose pt1 and pt2, then print alternative value of choosen pattern
 
-pt1=["*","#"]
-pt2=["$","%"]
-n=8             //input val- no of rows
+const pt1=["*","#"]
+const pt2=["$","%"]
+var n=8             //input val- no of rows
 for(let i=n;i>=1;i--){
-    k=""
-    alt=true
-    pt=i%2===0 ? pt2:pt1  
+    let k=""
+    let alt=true
+    let pt=i%2===0 ? pt2:pt1  
     //alternate between element 1 and 2 of pt
     for (let j=1;j<=i;j++){
-        val=alt?pt[0]:pt[1]
+        let val=alt?pt[0]:pt[1]
         k+=val+" "
         alt=alt?false:true
     }
-    space=" ".repeat(n-i)
-    nw=space+k
+    let space=" ".repeat(n-i)
+    let nw=space+k
     console.log(nw)
 }
